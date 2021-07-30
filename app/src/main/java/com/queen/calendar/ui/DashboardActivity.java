1917df5cefe0d8dc441c1d625d82f994db2b406a
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-07-30 05:53:04 +0300

    added new files

--- app/src/main/java/com/queen/calendar/ui/DashboardActivity.java
+++ app/src/main/java/com/queen/calendar/ui/DashboardActivity.java
@@ -2,21 +2,33 @@ package com.queen.calendar.ui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.app.ListActivity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.EventLog;
 import android.util.Log;
+import android.util.Pair;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.firebase.events.Event;
 import com.queen.calendar.R;
 
@@ -24,78 +36,69 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    Calendar calendar = Calendar.getInstance();
-    CalendarView calendarView;
-    Button mButn;
+    private Button mButn;
+    private TextView mTxt;
+    private ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_ativity);
 
-        calendarView = (CalendarView) findViewById(R.id.calendar);
-        mButn = (Button) findViewById(R.id.ctick);
-        calendarView.setClickable(true);
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar.clear();
 
-        calendarView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                ///Log.e("event", "click");
-            }
-        });
+        mButn = (Button) findViewById(R.id.bttn);
+        mTxt = (TextView) findViewById(R.id.txtDay);
+        imageButton = findViewById(R.id.imgbtn);
 
-        calendarView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Intent intent = new Intent(DashboardActivity.this, ListActivity.class);
-                Toast.makeText(DashboardActivity.this, "Period start", Toast.LENGTH_SHORT).show();
-                //Log.e("event", "click");
-                return false;
-            }
-        });
-
-        mButn.setOnClickListener(new View.OnClickListener() {
+        imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DashboardActivity.this, CalActivity.class);
                 startActivity(intent);
             }
         });
-//        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-//            @Override
-//            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-//                calendarView.setDate(5,true,true);
-//                Log.e("event", "datechanged");
-//            }
-//        });
 
-        //calendarView.setonCellClickListener()
+
+        long today = MaterialDatePicker.todayInUtcMilliseconds();
+
+        calendar.setTimeInMillis(today);
+
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        long january = calendar.getTimeInMillis();
+
+        calendar.set(Calendar.MONTH, Calendar.DECEMBER);
+        long december  = calendar.getTimeInMillis();
+
+        //calendarConstraints
+        CalendarConstraints.Builder constaraintBuilder = new CalendarConstraints.Builder();
+        constaraintBuilder.setStart(january);
+        constaraintBuilder.setEnd(december);
+
+
+        //materialdatePicker
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.dateRangePicker();
+        MaterialDatePicker materialDatePicker = builder.build();
+        builder.setTitleText("Start date");
+        builder.setSelection(today);
+        builder.setCalendarConstraints(constaraintBuilder.build());
+
+        mButn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker.show(getSupportFragmentManager(), "DATE_RANGE_PICKER");
+            }
+        });
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                mTxt.setText("Start date - end date :" + materialDatePicker.getHeaderText());
+            }
+        });
     }
-
-//    public Calendar getCalendar() {
-//        calendar.set(Calendar.DAY_OF_MONTH, 5);
-//        calendar.set(Calendar.HOUR, 0);
-//        calendar.set(Calendar.MINUTE, 0);
-//        calendar.set(Calendar.SECOND, 0);
-//        calendar.set(Calendar.MILLISECOND, 0);
-//
-//        long eventStart = calendar.getTimeInMillis();
-//        calendar.add(Calendar.DAY_OF_MONTH,5);
-//        long eventEnd = calendar.getTimeInMillis();
-//
-//        Event event = new Event(DashboardActivity.class, eventStart);
-//        List<Event> events = new ArrayList<Event>();
-//        events.add(event);
-//        calendarView.getEventAdapater();
-//
-//
-//        return calendar;
-//    }
-//
-//    public void setCalendarView(CalendarView calendarView) {
-//        this.calendarView = calendarView;
-//    }
-
 }
